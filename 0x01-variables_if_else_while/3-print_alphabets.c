4d1c9250daa14ffb42248e3bcc1ce51548583c7b
miyerima <ministeryerima@gmail.com>
2022-07-01 17:56:51 -0700

    fixed alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	char c = 'a';
-	while (c <= 'z')
+char c = 'a';
+
+while (c <= 'z')
 {
-	putchar(c);
-	c++;
+putchar(c);
+c++;
 }
 c = 'A';
 while (c <= 'Z')
 {
-	putchar(c);
-	c++;
+putchar(c);
+c++;
 }
 putchar('\n');
 return (0);
