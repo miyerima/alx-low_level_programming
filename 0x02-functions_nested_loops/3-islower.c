f18b51b9e24d72cf808ffd335adb6ba14a2e3de3
miyerima <ministeryerima@gmail.com>
2022-07-05 15:03:33 -0700

    fixed 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,10 @@
 /**
  * _islower - checks for lower case letters
  * @c : character to check the case
- * Retuen: 0 or 1
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
-return (c => 97 && c <= 122);
+return (c >= 97 && c <= 122);
+
 }
