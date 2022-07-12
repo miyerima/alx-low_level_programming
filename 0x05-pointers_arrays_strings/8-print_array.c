e1f5330b3b0583f19436c26591c33643e174cdfd
miyerima <ministeryerima@gmail.com>
2022-07-12 14:41:54 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- * print_array - print `n` elements of an array of integers
+/**
+ * print_array - print n elements of an array of integers
  * @a: int type array pointer
  * @n: int type integer
  * Description: Numbers must be separated by comma and space.
