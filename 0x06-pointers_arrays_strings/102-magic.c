ea46ec7a8dcda3310866ef8108bf9ed67ea8d575
miyerima <ministeryerima@gmail.com>
2022-07-15 16:12:52 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,14 +4,13 @@
  * main -> main function
  * Return: 0 success
  */
-
 int main(void)
 {
 	int n;
-	int a[5];
+	int c[5];
 	int *p;
 
-	a[2] = 1024;
+	c[2] = 1024;
 	p = &n;
 	/**
 	 * write your line of code here...
@@ -19,6 +18,6 @@ int main(void)
 	 */
 	p[5] = 98;
 	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("c[2] = %d\n", c[2]);
 	return (0);
 }
