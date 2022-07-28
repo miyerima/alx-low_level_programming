167a1e63041f02e308e07b1c3b2fd6691b0ec3f7
miyerima <ministeryerima@gmail.com>
2022-07-28 15:59:17 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,17 @@
+#ifndef _main_h
+#define _main_h
+
+#include <stdlib.h>
+#include <stddef.h>
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
+#endif
